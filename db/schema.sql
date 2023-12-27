5df39ed713dc27c38bcafa837de519e7f6d4c022
Tom Dossman <tdossman.development@gmail.com>
2023-12-27 13:26:48 -0500

    schema designed and created w/foreign keys

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS company_db; 
+CREATE DATABASE company_db; 
+USE company_db;
+
+CREATE TABLE departments (
+  department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+  department_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE roles (
+  role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  job_title VARCHAR(100) NOT NULL,
+  department_id INT NOT NULL, 
+  role_salary DECIMAL(12,2) NOT NULL,
+  FOREIGN KEY (department_id),
+  REFERENCES departments(department_id)
+);
+
+CREATE TABLE employees (
+  employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  employee_last_name VARCHAR(50) NOT NULL,
+  employee_first_name VARCHAR(50) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NULL,
+  FOREIGN KEY (role_id),
+  REFERENCES roles (role_id),
+  FOREIGN KEY (manager_id),
+  REFERENCES employees (employee_id)
+);
