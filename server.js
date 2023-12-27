798d2ed8caaa848d3166a76bbff168e40fea5b8e
Tom Dossman <tdossman.development@gmail.com>
2023-12-27 16:37:56 -0500

    mySQL queries for view alls complete

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+// Import dependencies
 const express = require('express');
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const table = require('table');
 
+// Set PORT
 const PORT = process.env.PORT || 3001;
+
+// Initialize instance of express.js
 const app = express();
 
 // Express middleware
@@ -16,7 +22,41 @@ const db = mysql.createConnection(
     user: 'root',
     // Add MySQL password
     password: 'Orca92',
-    database: 'books_db'
+    database: 'company_db'
   },
-  console.log(`Connected to the books_db database.`)
+  console.log(`Connected to the company database.`)
 );
+
+// view all departments
+db.query(
+  'SELECT * FROM departments',
+   function (err, results) {
+    console.log('Here are all company departments');
+    console.table(results);
+  }
+);
+
+// view all roles
+db.query(
+  `SELECT r.role_id, r.job_title, d.department_name, r.role_salary FROM roles r
+  JOIN departments d ON r.department_id = d.department_id;`, 
+  function (err, results) {
+    console.log('Here are all department roles');
+    console.table(results);
+  }
+);
+
+// view all employees
+db.query(
+  `SELECT e.employee_id, e.employee_first_name, e.employee_last_name, r.job_title, d.department_name, r.role_salary, CONCAT(m.employee_first_name, ' ', m.employee_last_name) AS manager_name
+  FROM employees e
+  JOIN roles r ON e.role_id = r.role_id
+  JOIN departments d ON r.department_id = d.department_id
+  LEFT JOIN employees m ON e.manager_id = m.employee_id
+  ;`,
+  function (err, results) {
+    console.log('Here are all employees');
+    console.table(results);
+  }
+);
+
