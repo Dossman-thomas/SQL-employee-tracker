a12ff46dad078e25901689e90979461a4ae26821
Tom Dossman <tdossman.development@gmail.com>
2023-12-30 15:46:23 -0500

    cfonts styled title added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const inquirer = require('inquirer');
 const table = require('table');
 const util = require('./db/dbUtility');
 const db = require('./db/db');
+const cfonts = require('cfonts');
 
 // Ensure the connection is established before calling init
 db.connect((err) => {
@@ -13,6 +14,29 @@ db.connect((err) => {
     init();
   }
 });
+
+// Function to create a styled title
+const createTitle = () => {
+  cfonts.say(' Employee Tracker ', {
+    font: 'shade', // You can choose a different font style
+    align: 'center', // define text alignment
+    colors: ['system', 'cyan'], // define all colors
+    background: 'transparent', // define background color
+    letterSpacing: 1, // define letter spacing
+    lineHeight: 1, // define the line height
+    space: true, // define if the output text should have empty lines on top and on the bottom
+    maxLength: '0', //define how many chars can be on a line
+    gradient: false, // define your two gradient colors
+    independentGradient: false, // define if you want to recalculate the gradient for each new line
+    transitionGradient: false, // define if this is a transition between colors directly
+    env: 'node' // define the environment cfonts is being executed in
+  });
+};
+
+// Call the function to create the title
+createTitle();
+
+
 
 // init function
 const init = () => {
